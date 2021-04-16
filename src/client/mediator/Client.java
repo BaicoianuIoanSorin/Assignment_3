8783b7c61621cd974489f4a246916a6a6945a537
rycka13 <71268611+rycka13@users.noreply.github.com>
2021-04-16 13:31:10 +0200

    startig with remote

--- src/client/mediator/Client.java
+++ src/client/mediator/Client.java
@@ -1,4 +1,42 @@
 package client.mediator;
 
-public class Client {
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public class Client implements RemoteModel{
+    @Override
+    public void getAllUsers() throws RemoteException {
+        
+    }
+
+    @Override
+    public void addLog(String log) throws IOException, RemoteException {
+
+    }
+
+    @Override
+    public ArrayList<String> getLog() throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public int getConnectedUsersInt() throws RemoteException {
+        return 0;
+    }
+
+    @Override
+    public void getConnectedUsers() throws RemoteException {
+
+    }
+
+    @Override
+    public void addUser(String name) throws RemoteException {
+
+    }
+
+    @Override
+    public void removeUser(String name) throws RemoteException {
+
+    }
 }
--- src/client/mediator/RemoteModel.java
+++ src/client/mediator/RemoteModel.java
@@ -1,4 +1,17 @@
 package client.mediator;
 
-public interface RemoteModel {
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface RemoteModel extends Remote {
+
+    void getAllUsers() throws RemoteException;
+    void addLog(String log) throws IOException,RemoteException;
+    ArrayList<String> getLog() throws RemoteException;
+    int getConnectedUsersInt() throws RemoteException;
+    void getConnectedUsers() throws RemoteException;
+    void addUser(String name) throws RemoteException;
+    void removeUser(String name) throws RemoteException;
 }
